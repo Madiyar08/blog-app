97ddc9dd6b33a8b986244a9dc5beb2a1360ba7c5
unknown <you@example.com>
2023-09-03 05:27:51 +0500

    post.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/signUp";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/post" element={<Post/>} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ src/pages/Post.jsx
@@ -0,0 +1,16 @@
+function Post() {
+  return (
+    <div className="post flex w-full h-full bg-[#272727]">
+      <div className="post-title">
+        <h1>15 Disadvantages Of Freedom And How You Can Workaround It.</h1>
+      </div>
+
+      <div className="post-author text-[#A5A5A5]">
+        <p>written by @samurai2099</p>
+        <p>on 27 may 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
