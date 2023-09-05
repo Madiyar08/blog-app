5eaaf6fc22d10a2f896f71b3510a07d1b2790b04
unknown <you@example.com>
2023-09-05 07:17:19 +0500

    formik.js added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Formik } from 'formik';
 import "../index.css";
 function Login() {
   return (
@@ -21,31 +22,62 @@ function Login() {
           </p>
         </div>
 
-        <form className="inputs flex flex-col space-y-[25px] mt-[40px] md:mt-[30px]">
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+        <form onSubmit={handleSubmit} className="inputs flex flex-col space-y-[25px] mt-[40px] md:mt-[30px]">
           <input
             className="w-full md:w-[70%] lg:w-[600px] h-[60px] text-[16px] font-[400] text-[#A5A5A5] py-[20px] pl-[30px] border-2 border-[#6EEB83] bg-[#272727] focus:outline-none transition duration-300 ease-in-out transform hover:scale-105"
             type="email"
-            name="email"
-            id="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
             placeholder="Enter your email"
           />
-
+        {errors.email && touched.email && errors.email}
           <input
             className="w-full md:w-[70%] lg:w-[600px] h-[60px] text-[16px] font-[400] text-[#A5A5A5] py-[20px] pl-[30px] border-2 border-[#6EEB83] bg-[#272727] focus:outline-none transition duration-300 ease-in-out transform hover:scale-105"
             type="password"
-            name="password"
-            id="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
             placeholder="Enter password"
           />
-        </form>
+        {errors.password && touched.password && errors.password}
 
         <div className="buttons flex flex-col md:flex-row justify-between mt-[22px]">
-          <a
-            className="text-center text-[20px] w-full md:w-[40%] lg:w-[auto] bg-[#6EEB83] py-[18px] px-[48px] font-[600] active:scale-95 duration-200 uppercase hover:bg-green-500 hover:text-white hover:rounded-5 hover:shadow-md hover:shadow-lg hover:shadow-xl hover:shadow-2xl md:w-auto md:mr-[20px] transition duration-300 ease-in-out transform hover:scale-105"
-            href="#"
-          >
-            LOGIN
-          </a>
+        <button className = "text-center text-[20px] w-full md:w-[40%] lg:w-[auto] bg-[#6EEB83] py-[18px] px-[48px] font-[600] active:scale-95 duration-200 uppercase hover:bg-green-500 hover:text-white hover:rounded-5 hover:shadow-md hover:shadow-lg hover:shadow-xl hover:shadow-2xl md:w-auto md:mr-[20px] transition duration-300 ease-in-out transform hover:scale-105" type="submit" disabled={isSubmitting}>
+             LOGIN
+           </button>
           <div className="mt-[20px] md:mt-0">
             <Link to={'/signUp'} className="text-[20px] font-[400] leading-normal text-white cursor-pointer">
               don’t have an account?
@@ -56,6 +88,10 @@ function Login() {
             </Link>
           </div>
         </div>
+        </form>
+       )}
+     </Formik>
+     
       </div>
     </div>
   );
